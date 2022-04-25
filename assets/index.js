557223d917642cd9695b54461ba7d6eb2b3dff6d
masontmorris <masonmorris8@gmail.com>
2022-04-24 22:58:58 -0400

    uh kinda wrote most of this in one go whoops

--- assets/index.js
+++ assets/index.js
@@ -82,7 +82,81 @@ function newIntern() {
 }
 
 function buildTeam() {
-    fs.writeFile("data/team.json", JSON.stringify(team, null, "\t"), function (err) {
+    let teamCards = "";
+    for (let i = 0; i < team.length; i++) {
+        if (team[i].role === "Manager") {
+            teamCards += `
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="card-header-title">${team[i].name}</h2>
+                        <br />
+                        <h3 class="card-content">${team[i].role}</h3>
+                    </div>
+                    <div class="card-content card-body">
+                        <ul>
+                            <li>ID: ${team[i].empID}</li>
+                            <li>Email: <a href="mailto:${team[i].email}" target="_blank">${team[i].email}</a></li>
+                            <li>Office number: ${team[i].officeNumber}</li>
+                        </ul>
+                    </div>
+                </div>
+                `;
+        } else if (team[i].role === "Engineer") {
+            teamCards += `
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="card-header-title">${team[i].name}</h2>
+                        <br />
+                        <h3 class="card-content">${team[i].role}</h3>
+                    </div>
+                    <div class="card-content card-body">
+                        <ul>
+                            <li>ID: ${team[i].empID}</li>
+                            <li>Email: <a href="mailto:${team[i].email}" target="_blank">${team[i].email}</a></li>
+                            <li>Github: <a href="https://www.github.com/${team[i].github}" target="_blank">${team[i].github}</a></li>
+                        </ul>
+                    </div>
+                </div>
+                `;
+        } else if (team[i].role === "Intern") {
+            teamCards += `
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="card-header-title">${team[i].name}</h2>
+                        <br />
+                        <h3 class="card-content">${team[i].role}</h3>
+                    </div>
+                    <div class="card-content card-body">
+                        <ul>
+                            <li>ID: ${team[i].empID}</li>
+                            <li>Email: <a href="mailto:${team[i].email}" target="_blank">${team[i].email}</a></li>
+                            <li>School: ${team[i].school}</li>
+                        </ul>
+                    </div>
+                </div>
+                `;
+        }
+    }
+    let teamHTML = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" />
+        <link rel="stylesheet" href="./assets/styles.css" />
+    </head>
+    <body>
+        <div class="header">
+        <h1>My Team</h1>
+        </div>
+        <div class = "flex-container">
+        ${teamCards}
+        </div>
+    </body>
+    </html>`;
+    fs.writeFile("index.html", teamHTML, function (err) {
         if (err) {
             return console.log(err);
         }
